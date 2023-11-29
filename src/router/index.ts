c6801457ba214dfa45686e4a365e0d4529a850db
danivdwerf <danivdwerf@gmail.com>
2023-11-29 09:53:07 +0100

    Setup empty router

---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+// Components
+
+export default createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: []
+});;
